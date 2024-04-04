6063c44853121b5a40c05c96372aaac4e37a49ae
EvinAltra <evinaltra@gmail.com>
2024-04-05 04:40:02 +0700

    Menambahkan file pustaka-booking-15220092

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+{
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya calvin altra 
+        saya tinggal di daerah bekasi
+        olahraga yang saya sukai adalah bulutangkis";
+}
+}
