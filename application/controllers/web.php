77e703acf514ec34d6ef0b897d2206f6ff59b4df
EvinAltra <evinaltra@gmail.com>
2024-04-17 19:32:59 +0700

    menambahkan web.php, v_header.php, v_about.php, v_index.php dan assets/css/stylebuku.css

--- application/controllers/web.php
+++ application/controllers/web.php
@@ -4,6 +4,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Web extends CI_Controller {
     function __construct() {
         parent::__construct();
+        $this->load->helper('url');
     }
 
     public function index() {
@@ -12,4 +13,12 @@ class Web extends CI_Controller {
         $this->load->view('v_index', $data);
         $this->load->view('v_footer', $data);
     }
+public function about()
+{
+    $data['judul'] = "Halaman About";
+    $this->load->view('v_header', $data);
+    $this->load->view('v_about', $data);
+    $this->load->view('v_footer', $data);
 }
+}
+
